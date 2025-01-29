e0bbbd916fb340e5c3e71f514b54a1013560e1ee
tincenv <tincenv@gmail.com>
2025-01-29 10:10:21 +0100

    Ajoutez des fichiers projet.

---
+++ snake/Program.cs
@@ -0,0 +1,22 @@
+﻿using Raylib_cs;
+class Program
+{
+    static void Main()
+    {
+        int screenWidth = 800;
+        int screenHeight = 600;
+
+        Raylib.InitWindow(screenWidth, screenHeight, "Snake Game");
+        Raylib.SetTargetFPS(60);
+
+
+        while (!Raylib.WindowShouldClose())
+        {
+            Raylib.BeginDrawing();
+            Raylib.ClearBackground(Color.White);
+            Raylib.DrawText("Snake Game", 10, 10, 20, Color.Black);
+            Raylib.EndDrawing();
+        }
+        Raylib.CloseWindow();
+    }
+}
